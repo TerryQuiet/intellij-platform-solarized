ddcf870b9b9ad31ebed001057a34698f412411b8
GitHub Action <action@github.com>
2024-08-03 10:12:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/terryquiet/quietlysolarized/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/terryquiet/quietlysolarized/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.terryquiet.quietlysolarized.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/terryquiet/quietlysolarized/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/terryquiet/quietlysolarized/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.terryquiet.quietlysolarized.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.terryquiet.quietlysolarized.MyBundle
+import com.github.terryquiet.quietlysolarized.services.MyProjectService
 import javax.swing.JButton
 
 
